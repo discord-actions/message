507c9b6938aa29ebe2405dee1c26f34baaf4dee7
Jack Allen <jackallensemail+git@gmail.com>
2024-01-23 13:53:08 +0000

    build

--- index.ts
+++ index.ts
@@ -2,11 +2,13 @@ import core from "@actions/core";
 import github from "@actions/github";
 
 async function main() {
+  console.log("github->discord");
+
   const webhookUrl = core.getInput("webhookUrl", {
     trimWhitespace: true,
   });
 
-
+  console.log({ webhookUrl });
 }
 
 main().then();
